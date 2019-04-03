31e3074f60f8d086e342d0fa79f61f70aed82fd2
yangjun <290968698@qq.com>
2019-04-03 09:53:18 +0800

    add interface for rpc forward

--- stream.go
+++ stream.go
@@ -112,6 +112,7 @@ type ClientStream interface {
 	// to call SendMsg on the same stream in different goroutines. It is also
 	// not safe to call CloseSend concurrently with SendMsg.
 	SendMsg(m interface{}) error
+	SendMsgForward(mBytes []byte) error
 	// RecvMsg blocks until it receives a message into m or the stream is
 	// done. It returns io.EOF when the stream completes successfully. On
 	// any other error, the stream is aborted and the error contains the RPC
@@ -121,6 +122,7 @@ type ClientStream interface {
 	// calling RecvMsg on the same stream at the same time, but it is not
 	// safe to call RecvMsg on the same stream in different goroutines.
 	RecvMsg(m interface{}) error
+	RecvMsgForward() ([]byte, error)
 }
 
 // NewStream creates a new Stream for the client side. This is typically
